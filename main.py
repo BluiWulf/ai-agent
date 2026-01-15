459139e2213529442012605413a5f728e4a4d8c1
BluiWulf <bluiwulf@gmail.com>
2026-01-14 19:37:13 -0500

    Finished CH1 of AI-Agent guided project from Boot.dev

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
-def main():
-    print("Hello from ai-agent!")
+import os
+import argparse
 
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key is None:
+        raise RuntimeError("API Key is missing")
+    
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User Prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+    user_prompt = args.user_prompt
+
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
+    
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model='gemini-2.5-flash', contents=messages
+    )
+
+    if response.usage_metadata is None:
+        raise RuntimeError("Failed API request")
+    
+    if args.verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response:\n{response.text}")
 
 if __name__ == "__main__":
     main()
