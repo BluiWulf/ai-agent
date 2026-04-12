66239e89dc72e4b60aec162cb1eb6f589abd008d
BluiWulf <bluiwulf@gmail.com>
2026-04-11 22:16:02 -0400

    Updating main

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
+
+from functions.get_files_info import schema_get_files_info
+from functions.get_file_content import schema_get_file_content
+from functions.run_python_file import schema_run_python_file
+from functions.write_file import schema_write_file
 
 def main():
     load_dotenv()
@@ -20,8 +26,18 @@ def main():
     messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
     
     client = genai.Client(api_key=api_key)
+    avail_functions = types.Tool(
+        function_declarations=[schema_get_files_info, schema_get_file_content, schema_run_python_file, schema_write_file]
+    )
+    aiConfig = types.GenerateContentConfig(
+        tools=[avail_functions],
+        system_instruction=system_prompt,
+        temperature=0
+    )
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents=messages
+        model='gemini-1.5-flash',
+        contents=messages,
+        config=aiConfig,
     )
 
     if response.usage_metadata is None:
@@ -31,7 +47,12 @@ def main():
         print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print(f"Response:\n{response.text}")
+    
+    if response.function_calls:
+        for call in response.function_calls:
+            print(f"Calling function: {call.name}({call.args})")
+    else:
+        print(f"Response:\n{response.text}")
 
 if __name__ == "__main__":
     main()
